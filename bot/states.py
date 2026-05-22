1c05c4978320b353566b464e382da50deb33842b
nexuswirsok <44503801+nexuswirsok@users.noreply.github.com>
2026-05-22 16:33:48 +0300

    Update states.py

--- bot/states.py
+++ bot/states.py
@@ -1,1 +1,14 @@
-#
+from aiogram.fsm.state import State, StatesGroup
+
+
+class AddOrder(StatesGroup):
+    order_type = State()
+    fio = State()
+    phone = State()
+    printer = State()
+    description = State()
+    price = State()
+    accept_date = State()
+    visit_date = State()
+    master = State()
+    search = State()

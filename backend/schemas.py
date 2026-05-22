84c251502227cb4d62dfdd72f44b167755d803c1
nexuswirsok <44503801+nexuswirsok@users.noreply.github.com>
2026-05-22 16:31:18 +0300

    Update schemas.py

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,1 +1,18 @@
-#
+from pydantic import BaseModel
+
+
+class OrderCreate(BaseModel):
+    order_type: str
+    fio: str
+    phone: str
+    printer: str
+    description: str
+    price: str
+    accept_date: str
+    visit_date: str
+    master: str
+    status: str = "Принят"
+
+
+class StatusUpdate(BaseModel):
+    status: str

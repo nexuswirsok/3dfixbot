71076abbdf38cc0d0dd952dd045b2aa0566ed45f
nexuswirsok <44503801+nexuswirsok@users.noreply.github.com>
2026-05-22 16:31:03 +0300

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,31 @@
-#
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+
+Base = declarative_base()
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    order_type = Column(String)
+
+    fio = Column(String)
+
+    phone = Column(String)
+
+    printer = Column(String)
+
+    description = Column(String)
+
+    price = Column(String)
+
+    accept_date = Column(String)
+
+    visit_date = Column(String)
+
+    master = Column(String)
+
+    status = Column(String)

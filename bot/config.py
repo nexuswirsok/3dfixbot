f0b05355c961b68d7db04548a447dba7d425a333
nexuswirsok <44503801+nexuswirsok@users.noreply.github.com>
2026-05-22 16:32:40 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,1 +1,15 @@
-#
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+TOKEN = os.getenv("TOKEN")
+
+BACKEND_URL = os.getenv("BACKEND_URL")
+
+ADMIN_ID = os.getenv("ADMIN_ID")
+
+MASTER_IDS = os.getenv("MASTER_IDS", "")
